0cef6cf2002f1029a1b7f87cd536046242ba724e
vuxqzo <you@example.com>
2026-01-26 01:33:17 +0100

    Security checks

--- Roblox-Injector/main.cpp
+++ Roblox-Injector/main.cpp
@@ -13,21 +13,27 @@ std::string SignModule(std::string ModulePath)
     char TempPath[MAX_PATH]{};
     GetTempPathA(MAX_PATH, TempPath);
     std::string SignedModulePath = std::string(TempPath) + "vcruntime140.dll";
-    std::filesystem::remove(SignedModulePath);
-    std::filesystem::copy(ModulePath, SignedModulePath);
 
-    CkCert Cert;
-    CkByteData PfxBd;
-    std::vector<uint8_t> Pfx(PfxData, PfxData + sizeof(PfxData));
-    PfxBd.append2(Pfx.data(), static_cast<unsigned long>(Pfx.size()));
-    Cert.LoadPfxData(PfxBd, "NG");
+    DeleteFileA(SignedModulePath.c_str());
+    if (!std::filesystem::exists(SignedModulePath))
+    {
+        std::filesystem::copy(ModulePath, SignedModulePath);
 
-    CkCodeSign CodeSign;
-    CkJsonObject Options;
-    Options.UpdateString("hashAlgorithm", "SHA256");
-    CodeSign.AddSignature(SignedModulePath.c_str(), Cert, Options);
+        CkCert Cert;
+        CkByteData PfxBd;
+        std::vector<uint8_t> Pfx(PfxData, PfxData + sizeof(PfxData));
+        PfxBd.append2(Pfx.data(), static_cast<unsigned long>(Pfx.size()));
+        Cert.LoadPfxData(PfxBd, "NG");
 
-    return SignedModulePath;
+        CkCodeSign CodeSign;
+        CkJsonObject Options;
+        Options.UpdateString("hashAlgorithm", "SHA256");
+        CodeSign.AddSignature(SignedModulePath.c_str(), Cert, Options);
+
+        return SignedModulePath;
+    }
+    
+    return "";
 }
 
 int main()
@@ -52,6 +58,13 @@ int main()
     }
 
     std::string SignedModule = SignModule("Module.dll");
+    if (SignedModule.empty())
+    {
+        std::cout << "\n [/] Failed to delete old Module or youre already injected";
+        Sleep(2500);
+        return 0;
+    }
+
     DWORD ProcessID, ThreadID = GetWindowThreadProcessId(hWindow, &ProcessID);
     HMODULE hModule = LoadLibraryExA(SignedModule.c_str(), NULL, DONT_RESOLVE_DLL_REFERENCES);
     if (!GetProcAddress(hModule, "_CreateFrameInfo"))
